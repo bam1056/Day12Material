10085ed1ff7a1578f8203730ba29bd56a7a80880
Brett Macy <bam03584@aol.com>
2016-08-03 17:16:08 -0400

    Got logic and modal working

--- main.js
+++ main.js
@@ -8,22 +8,50 @@ const X = 1
 const O = 2
 const cellValues = { 0: '', 1: 'X', 2: 'O' }
 let playerTurn = X
+let winner = false
+
 const drawBoard = () => {
   for (let i = 0; i < state.length; i++) {
     for (let j = 0; j < state[i].length; j++) {
       const col = state[i][j]
       const cell = document.querySelector(
-        `table tr:nth-child(${i+1}) td:nth-child(${j+1})`
+        `table tr:nth-child(${i + 1}) td:nth-child(${j + 1}).clicked`
       )
       cell.textContent = cellValues[col]
     }
   }
   document.querySelector('.message').textContent = `It's ${cellValues[playerTurn]}'s turn.`
+  if (winner) { document.querySelector('.modal.hidden').className = 'modal' }
 }
 
-const play = (row, col, player) => {
-  state[row][col] = player
-  playerTurn = player === X ? O : X
+const play = (row, col) => {
+  if (state[row][col] === 0) {
+    state[row][col] = playerTurn
+    if (checkForWinner()) {
+      winner = true
+      drawBoard()
+      console.log(playerTurn + ' WINS!')
+    }
+    else playerTurn = playerTurn === X ? O : X
+  }
+}
+
+const checkForWinner = () => {
+  let arr = [
+    [state[0][0], state[0][1], state[0][2]],
+    [state[1][0], state[1][1], state[1][2]],
+    [state[2][0], state[2][1], state[2][2]],
+    [state[0][0], state[1][0], state[2][0]],
+    [state[0][1], state[1][1], state[2][1]],
+    [state[0][2], state[1][2], state[2][2]],
+    [state[0][0], state[1][1], state[2][2]],
+    [state[0][2], state[1][1], state[2][0]]
+  ]
+
+  for (let i = 0; i < arr.length; i++) {
+    let won = arr[i].every(function (m) { return m === playerTurn })
+    if (won) { return true }
+  }
 }
 
 const init = () => {
@@ -32,7 +60,7 @@ const init = () => {
     const cols = rows[i].querySelectorAll('td')
     for (let j = 0; j < cols.length; j++) {
       cols[j].addEventListener('click', () => {
-        play(i, j, playerTurn)
+        play(i, j)
         drawBoard()
       })
     }
